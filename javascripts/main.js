ae0f895c412e08cbbda239ee828276f12e88cf98
Chris Cortez <chris@polymathic.me>
2016-06-03 15:43:15 -0500

    add custom javascript functions

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,9 @@
-console.log('This would be the main JS file.');
+function HB_BEFORE() {
+  console.log("hb before");
+  HB.setCustomConditionValue('cond', 'false');
+}
+
+function HB_READY() {
+  console.log("hb ready");
+  HB.setCustomConditionValue('cond', 'true');
+}
